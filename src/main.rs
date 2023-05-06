6e2226293fd5bd0270818229e775e0176dd33600
Hasan Hakkaev <hasan.hakkaev@gmx.at>
2023-05-06 21:01:29 +0200

    added date

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,8 @@ fn main() {
     println!("Hello, world!");
     // print time and date
     print!("The time is {}", chrono::Local::now().format("%H:%M:%S"));
+    print!(
+        " and the date is {}",
+        chrono::Local::now().format("%Y-%m-%d")
+    );
 }
